c46b26e3e00f95cca4b62389c58a0863252065d2
Patryk Soika <patryk@superrenaissanceman.com>
2018-10-18 13:08:03 -0400

    re-knit everthing, just to be sure.

--- PUBLISH.sh
+++ PUBLISH.sh
@@ -2,6 +2,9 @@
 
 cd ~/Documents/other-git-repos/radix-source
 
+rm docs/PUBLISH.sh
+rsync -avP ./docs/ ../friendimaginary.github.io/
+
 git -C ../friendimaginary.github.io/ add .
 git -C ../friendimaginary.github.io/ commit -m "updated by PUBLISH.sh at $(date)."
 git -C ../friendimaginary.github.io/ push
